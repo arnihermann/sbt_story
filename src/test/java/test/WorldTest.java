88ee897370f6631c92b199078723b7a6c501b31a
Árni Hermann Reynisson <arnihr@gmail.com>
2013-10-19 21:38:42 +0000

    Added a failing test case.

--- src/test/java/test/WorldTest.java
+++ src/test/java/test/WorldTest.java
@@ -3,17 +3,13 @@ package test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
 public class WorldTest {
 
     @Test
     public void greetResultsInHello() {
         World world = new World();
-        assertEquals("Hello world", world.greet());
+        assertEquals("Hello word", world.greet());
     }
 
 }
