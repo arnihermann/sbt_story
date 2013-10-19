b2bf6560d75617bccff4bd677fb0a051d0dd37de
Árni Hermann Reynisson <arnihr@gmail.com>
2013-10-19 21:27:25 +0000

    Factor greeting into World class.

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -2,6 +2,7 @@ package test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        World world = new World();
+        System.out.println(world.greet());
     }
 }
---
+++ src/main/java/test/World.java
@@ -0,0 +1,7 @@
+package test;
+
+public class World {
+    public String greet() {
+        return "Hello world";
+    }
+}
